496b2dbb377bd3091b7c074a7463ee16776ac387
thomasrrgsd <thomasjarratt@gmail.com>
2022-03-10 15:05:01 -0500

    Added logger object.

--- william/bcs.py
+++ william/bcs.py
@@ -1,6 +1,6 @@
 #################################
 #				#
-# William T. Jarratt		#
+# PyROOT BCS Script             #
 # 			       	#
 #				#
 ################################# 
@@ -14,15 +14,27 @@ import time
 sys.path.append(".")
 from config import configuration
 
-format = "%(asctime)s: %(message)s"
-file_handler = logging.FileHandler(filename="temp.log")
-stdout_handler = logging.StreamHandler(sys.stdout)
-handlers = [file_handler, stdout_handler]
+# Create a basic logger and a file/stdout logger.
+log1 = logging.getLogger('console_log')
+log1.setLevel(logging.INFO)
 
-logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-file_handler.setFormatter(logging.Formatter(format, datefmt="%H:%M:%S"))
-logging.getLogger().addHandler(file_handler)
-#logging.getLogger().addHandler(stdout_handler)
+log2 = logging.getLogger('file_log')
+log2.setLevel(logging.INFO)
+
+format = "%(asctime)s: %(message)s"
+formatter = logging.Formatter(format, datefmt="%H:%M:%S")
+
+fh = logging.FileHandler(filename="temp.log")
+fh.setLevel(logging.INFO)
+fh.setFormatter(formatter)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+
+log1.addHandler(ch)
+log2.addHandler(fh)
+log2.addHandler(ch)
 
 config = configuration()
 
@@ -32,5 +44,6 @@ print(config.tree_in[1])
 print(config.tree_in[3])
 print(config)
 
-logging.info("this is a message")
+log1.info("this is a log1 message")
+log2.info("this is a log2 message")
 
